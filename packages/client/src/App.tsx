8717bd2a16b16b59fad35debdddac88004a34a72
Yauhen Kviatkouski <kvetkovski@gmail.com>
2021-04-03 21:02:48 +0300

    bind client and server

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [foo, setFoo] = useState("N/A");
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => {
+        console.log("🚀 ~ file: App.tsx ~ line 11 ~ .then ~ res", res);
+
+        return res.json();
+      })
+      .then((data) => {
+        console.log("🚀 ~ file: App.tsx ~ line 12 ~ .then ~ data", data);
+        setFoo(data.message);
+      })
+      .catch((err) => setFoo(err.message));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. \n message from
+          server: {foo}
         </p>
         <a
           className="App-link"
