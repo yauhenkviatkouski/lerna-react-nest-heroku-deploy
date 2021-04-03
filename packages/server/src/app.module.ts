5c949e24a9068c56a32d7815369f73c275b4ad4e
Yauhen Kviatkouski <kvetkovski@gmail.com>
2021-04-03 23:10:39 +0300

    serve static client

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const imports = [];
+
+if (process.env.NODE_ENV === 'production') {
+  imports.push(
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../client', 'build'),
+      exclude: ['/api*'],
+    }),
+  );
+}
 @Module({
-  imports: [],
+  imports,
   controllers: [AppController],
   providers: [AppService],
 })
